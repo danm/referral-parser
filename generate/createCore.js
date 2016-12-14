692a24a2aedda5961dc1afa5be65f03fb1c57c4d
Daniel Morrison <daniel.morrison@bbc.co.uk>
2016-12-14 23:28:27 +0000

    merged internal and master

--- generate/createCore.js
+++ generate/createCore.js
@@ -1,172 +1,173 @@
 'use strict';
 
-const request			= require('request');
-const yaml				= require('js-yaml');
-const fs				= require('fs');
+const request = require('request');
+const yaml = require('js-yaml');
+const fs = require('fs');
 
-let data 				= {};
-let search 				= {};
-let social 				= {};
-let other 				= {};
-let mail 				= {};
-let unknown 			= {};
+let data = {};
+let search = {};
+let social = {};
+let other = {};
+let mail = {};
+let unknown = {};
 
 //download 3 different files and combine them
 
 
 const getSearch = () => {
-	return new Promise((fulfill, reject) => {
-		request('https://raw.githubusercontent.com/piwik/searchengine-and-social-list/master/SearchEngines.yml', (err, res, body) => {
-				if (err) {
-					reject(err)
-				} else {
-					search = body;
-					fulfill();
-				}
-		});
-	})
+    return new Promise((fulfill, reject) => {
+        request('https://raw.githubusercontent.com/piwik/searchengine-and-social-list/master/SearchEngines.yml', (err, res, body) => {
+            if (err) {
+                reject(err);
+            } else {
+                search = body;
+                fulfill();
+            }
+        });
+    });
 };
 
 const getSocial = () => {
-	return new Promise((fulfill, reject) => {
-		request('https://raw.githubusercontent.com/piwik/searchengine-and-social-list/master/Socials.yml', (err, res, body) => {
-				if (err) {
-					reject(err)
-				} else {
-					social = body;
-					fulfill();
-				}
-		});
-	})
+    return new Promise((fulfill, reject) => {
+        request('https://raw.githubusercontent.com/piwik/searchengine-and-social-list/master/Socials.yml', (err, res, body) => {
+            if (err) {
+                reject(err);
+            } else {
+                social = body;
+                fulfill();
+            }
+        });
+    });
 };
 
 const getOther = () => {
-	return new Promise((fulfill, reject) => {
+    return new Promise((fulfill, reject) => {
 
-		// fs.readFile("../data/snowplow.yml", (err, data) => {
-		// 	if (err) {
-		// 		reject(err)
-		// 	} else {
-		// 		other = data.toString();
-		// 		fulfill();
-		// 	}
-		// });
+        fs.readFile("../data/snowplow.yml", (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                other = data.toString();
+                fulfill();
+            }
+        });
 
-		request('https://raw.githubusercontent.com/snowplow/referer-parser/master/resources/referers.yml', (err, res, body) => {
-				if (err) {
-					reject(err)
-				} else {
-					other = body;
-					fulfill();
-				}
-		});
-	})
+        // request('https://raw.githubusercontent.com/snowplow/referer-parser/master/resources/referers.yml', (err, res, body) => {
+        // 		if (err) {
+        // 			reject(err)
+        // 		} else {
+        // 			other = body;
+        // 			fulfill();
+        // 		}
+        // });
+    });
 };
 
 const combine = () => {
-	
-	//handle errors of YAML to JSON
-	try {
-		var otherJSON = yaml.safeLoad(other);
-	} catch (e) {
-		console.log(e);
-	}
 
-	try {
-		data.search = yaml.safeLoad(search);
-	} catch (e) {
-		console.log(e);
-	}
+    //handle errors of YAML to JSON
+    let otherJSON;
+    try {
+        otherJSON = yaml.safeLoad(other);
+    } catch (e) {
+        console.log(e);
+    }
 
-	try {
-		data.social = yaml.safeLoad(social);
-	} catch (e) {
-		console.log(e);
-	}
-	
-	data.uknown = otherJSON.unknown;
-	data.email 	= otherJSON.email;
+    try {
+        data.search = yaml.safeLoad(search);
+    } catch (e) {
+        console.log(e);
+    }
 
-	//combine lists
-	let found = false;
-	for (let social in otherJSON.social) {
-		found = false;
-		for (let site in data.social) {
-			if (social === site) {
-				found = true;
-			}
-		}
+    try {
+        data.social = yaml.safeLoad(social);
+    } catch (e) {
+        console.log(e);
+    }
 
-		if (found === false) {
-			data.social[social] = otherJSON.social[social];
-		}
-	}
-	
+    data.uknown = otherJSON.unknown;
+    data.email = otherJSON.email;
 
-	for (let search in otherJSON.search) {
+    //combine lists
+    let found = false;
+    for (let social in otherJSON.social) {
+        found = false;
+        for (let site in data.social) {
+            if (social === site) {
+                found = true;
+            }
+        }
 
-		found = false;
-		
-		for (let site in data.search) {
-			if (search === site) {
-				found = true;
-			}
-		}
+        if (found === false) {
+            data.social[social] = otherJSON.social[social];
+        }
+    }
 
-		if (found === false) {
-			data.search[search] = otherJSON.search[search];
-		}
-	}
+
+    for (let search in otherJSON.search) {
+
+        found = false;
+
+        for (let site in data.search) {
+            if (search === site) {
+                found = true;
+            }
+        }
+
+        if (found === false) {
+            data.search[search] = otherJSON.search[search];
+        }
+    }
 
 };
 
 const refactor = () => {
 
-	//remove 0 array from seach data to put at root
-	for (let domain in data.search) {
-		data.search[domain] = data.search[domain][0];
-	}
+    //remove 0 array from seach data to put at root
+    for (let domain in data.search) {
+        //facebook shouldn't be in search'
+        if (domain !== 'Facebook') {
+            data.search[domain] = data.search[domain][0];
+        } else {
+            delete data.search[domain];
+        }
+    }
 
-	//add urls object to social
-	for (let domain in data.social) {
-		data.social[domain] =  {
-			urls: data.social[domain]
-		}
-	}
-	
-	//change domains to urls
-	for (let domain in data.uknown) {
-		data.uknown[domain] =  {
-			urls: data.uknown[domain].domains
-		}
-	}
+    //add urls object to social
+    for (let domain in data.social) {
+        data.social[domain] = {
+            urls: data.social[domain]
+        };
+    }
 
-	//change domains to urls
-	for (let domain in data.email) {
-		data.email[domain] =  {
-			urls: data.email[domain].domains
-		}
-	}
+    //change domains to urls
+    for (let domain in data.uknown) {
+        data.uknown[domain] = {
+            urls: data.uknown[domain].domains
+        };
+    }
 
-	//Write to JSON File
-	fs.writeFile('../data/core.json', JSON.stringify(data));
-	console.log('done')
+    //change domains to urls
+    for (let domain in data.email) {
+        data.email[domain] = {
+            urls: data.email[domain].domains
+        };
+    }
 
-	
+    //Write to JSON File
+    fs.writeFile('../data/core.json', JSON.stringify(data));
+    console.log('done');
+
+
 };
 
 getSearch()
-.then(getSearch)
-.then(getSocial)
-.then(getOther)
-.then(function() {
+    .then(getSearch)
+    .then(getSocial)
+    .then(getOther)
+    .then(function() {
 
-	combine();
-	refactor();
+        combine();
+        refactor();
 
-});
-
-
-
-
-
+    });
--- test/referrerparse.js
+++ test/referrerparse.js
@@ -1,169 +1,251 @@
 'use strict';
 
-const mocha 	= require('mocha');
-const chai 		= require('chai');
-const expect 	= chai.expect;
-const Referrer  = require('../index');
+const mocha = require('mocha');
+const chai = require('chai');
+const expect = chai.expect;
+const Referrer = require('../index');
 
+const opts = { bbc: true };
 
 describe("Error Checking", function() {
-	it('should throw error if no referrer passed', function() {
-		//arrange
-		//assert
-		expect(function() {
-			//act
-			let res = new Referrer();
-		}).to.throw(/No referral URL found/);;
-	});
+    it('should throw error if no referrer passed', function() {
+        //arrange
+        //assert
+        expect(function() {
+            //act
+            let res = new Referrer();
+        }).to.throw(/No referral URL found/);;
+    });
 
-	it('should fix urls without protocol', function() {
-		//arrange
-		let ref = 'hello.is';
-		let obj = new Referrer(ref);
-		//assert
-		expect(obj).to.have.all.keys('refferal');
-		expect(obj.refferal).to.have.all.keys('host');
-		expect(obj.refferal.host).to.eql('hello.is');
-	});
+    it('should fix urls without protocol', function() {
+        //arrange
+        let ref = 'hello.is';
+        let obj = new Referrer(ref);
+        //assert
+        expect(obj).to.have.all.keys('refferal');
+        expect(obj.refferal).to.have.all.keys('host');
+        expect(obj.refferal.host).to.eql('hello.is');
+    });
 });
 
 describe("Unreconised Site", function() {
-	it('should return only host if site is not reconised', function() {
-		//arrange
-		let ref = 'http://hello.is/dsfsd';
-		let obj = new Referrer(ref);
-		//assert
-		expect(obj).to.have.all.keys('refferal');
-		expect(obj.refferal).to.have.all.keys('host');
-		expect(obj.refferal.host).to.eql('hello.is');
+    it('should return only host if site is not reconised', function() {
+        //arrange
+        let ref = 'http://hello.is/dsfsd';
+        let obj = new Referrer(ref);
+        //assert
+        expect(obj).to.have.all.keys('refferal');
+        expect(obj.refferal).to.have.all.keys('host');
+        expect(obj.refferal.host).to.eql('hello.is');
 
-	});
+    });
 });
 
 describe("Search", function() {
-	it('should return all properties for google.com with hash', function() {
-		//arrange
-		let ref = 'http://www.google.com/#q=bbc+news';
-		let obj = new Referrer(ref);
-		//assert
-		var ex = {
-			type: 'search',
-     		name: 'Google',
-     		search: 'bbc news',
-     		host: 'www.google.com' 
-		}
-		expect(obj).to.have.all.keys('refferal');
-		expect(obj.refferal).to.eql(ex);
-	});
+    it('should return all properties for google.com with hash', function() {
+        //arrange
+        let ref = 'http://www.google.com/#q=bbc+news';
+        let obj = new Referrer(ref);
+        //assert
+        var ex = {
+            type: 'search',
+            name: 'Google',
+            search: 'bbc news',
+            host: 'www.google.com'
+        }
+        expect(obj).to.have.all.keys('refferal');
+        expect(obj.refferal).to.eql(ex);
+    });
 
-	it('should return all properties for google.com with param', function() {
-		//arrange
-		let ref = 'http://www.google.com?q=bbc+news';
-		let obj = new Referrer(ref);
-		//assert
-		var ex = {
-			type: 'search',
-     		name: 'Google',
-     		search: 'bbc news',
-     		host: 'www.google.com' 
-		}
-		expect(obj).to.have.all.keys('refferal');
-		expect(obj.refferal).to.eql(ex);
-	});
+    it('should return all properties for google.com with param', function() {
+        //arrange
+        let ref = 'http://www.google.com?q=bbc+news';
+        let obj = new Referrer(ref);
+        //assert
+        var ex = {
+            type: 'search',
+            name: 'Google',
+            search: 'bbc news',
+            host: 'www.google.com'
+        }
+        expect(obj).to.have.all.keys('refferal');
+        expect(obj.refferal).to.eql(ex);
+    });
 
-	it('should return all properties for bing.com with param', function() {
-		//arrange
-		let ref = 'http://www.bing.com/search?q=bbc+news&go=Submit&qs=n&form=QBLH&pq=bbc+new&sc=1-0&sp=-1&sk=&cvid=2817C11E5B6D4A829A196DAC45EB457D';
-		let obj = new Referrer(ref);
-		//assert
-		var ex = {
-			type: 'search',
-     		name: 'Bing',
-     		search: 'bbc news',
-     		host: 'www.bing.com' 
-		}
-		expect(obj).to.have.all.keys('refferal');
-		expect(obj.refferal).to.eql(ex);
-	});
+    it('should return all properties for bing.com with param', function() {
+        //arrange
+        let ref = 'http://www.bing.com/search?q=bbc+news&go=Submit&qs=n&form=QBLH&pq=bbc+new&sc=1-0&sp=-1&sk=&cvid=2817C11E5B6D4A829A196DAC45EB457D';
+        let obj = new Referrer(ref);
+        //assert
+        var ex = {
+            type: 'search',
+            name: 'Bing',
+            search: 'bbc news',
+            host: 'www.bing.com'
+        }
+        expect(obj).to.have.all.keys('refferal');
+        expect(obj.refferal).to.eql(ex);
+    });
 });
 
 describe("Social", function() {
-	it('should return all properties for Facebook', function() {
-		//arrange
-		let ref = 'http://www.facebook.com';
-		let obj = new Referrer(ref);
-		//assert
-		var ex = {
-			type: 'social',
-     		name: 'Facebook',
-     		host: 'www.facebook.com' 
-		}
-		expect(obj).to.have.all.keys('refferal');
-		expect(obj.refferal).to.eql(ex);
-	});
+    it('should return all properties for Facebook', function() {
+        //arrange
+        let ref = 'http://www.facebook.com';
+        let obj = new Referrer(ref);
+        //assert
+        var ex = {
+            type: 'social',
+            name: 'Facebook',
+            host: 'www.facebook.com'
+        }
+        expect(obj).to.have.all.keys('refferal');
+        expect(obj.refferal).to.eql(ex);
+    });
 
-	it('should return all properties for Google Plus', function() {
-		//arrange
-		let ref = 'http://plus.google.com';
-		let obj = new Referrer(ref);
-		//assert
-		var ex = {
-			type: 'social',
-     		name: 'Google%2B',
-     		host: 'plus.google.com' 
-		}
-		expect(obj).to.have.all.keys('refferal');
-		expect(obj.refferal).to.eql(ex);
-	});
+    it('should return all properties for Google Plus', function() {
+        //arrange
+        let ref = 'http://plus.google.com';
+        let obj = new Referrer(ref);
+        //assert
+        var ex = {
+            type: 'social',
+            name: 'Google%2B',
+            host: 'plus.google.com'
+        }
+        expect(obj).to.have.all.keys('refferal');
+        expect(obj.refferal).to.eql(ex);
+    });
 });
 
 describe("News", function() {
-	it('should return type news for Flipboard', function() {
-		//arrange
-		let ref = 'https://flipboard.com';
-		let obj = new Referrer(ref);
-		//assert
-		var ex = {
-			type: 'news',
-     		name: 'Flipboard',
-     		host: 'flipboard.com' 
-		}
-		expect(obj).to.have.all.keys('refferal');
-		expect(obj.refferal).to.eql(ex);
-	});
+    it('should return type news for Flipboard', function() {
+        //arrange
+        let ref = 'https://flipboard.com';
+        let obj = new Referrer(ref);
+        //assert
+        var ex = {
+            type: 'news',
+            name: 'Flipboard',
+            host: 'flipboard.com'
+        }
+        expect(obj).to.have.all.keys('refferal');
+        expect(obj.refferal).to.eql(ex);
+    });
 
 });
 
 describe("Top level domains", function() {
-	it('Check google runs from non ISO TLD', function() {
-		//arrange
-		let ref = 'http://www.google.co.uk/#q=bbc+news';
-		let obj = new Referrer(ref);
-		//assert
-		var ex = {
-			type: 'search',
-     		name: 'Google',
-     		search: 'bbc news',
-     		host: 'www.google.co.uk' 
-		}
-		expect(obj).to.have.all.keys('refferal');
-		expect(obj.refferal).to.eql(ex);
-	});
+    it('Check google runs from non ISO TLD', function() {
+        //arrange
+        let ref = 'http://www.google.co.uk/#q=bbc+news';
+        let obj = new Referrer(ref);
+        //assert
+        var ex = {
+            type: 'search',
+            name: 'Google',
+            search: 'bbc news',
+            host: 'www.google.co.uk'
+        }
+        expect(obj).to.have.all.keys('refferal');
+        expect(obj.refferal).to.eql(ex);
+    });
 
-	it('Check google runs from ISO TLD', function() {
-		//arrange
-		let ref = 'http://www.google.ca/#q=bbc+news';
-		let obj = new Referrer(ref);
-		//assert
-		var ex = {
-			type: 'search',
-     		name: 'Google',
-     		search: 'bbc news',
-     		host: 'www.google.ca' 
-		}
-		expect(obj).to.have.all.keys('refferal');
-		expect(obj.refferal).to.eql(ex);
-	});
+    it('Check google runs from ISO TLD', function() {
+        //arrange
+        let ref = 'http://www.google.ca/#q=bbc+news';
+        let obj = new Referrer(ref);
+        //assert
+        var ex = {
+            type: 'search',
+            name: 'Google',
+            search: 'bbc news',
+            host: 'www.google.ca'
+        }
+        expect(obj).to.have.all.keys('refferal');
+        expect(obj.refferal).to.eql(ex);
+    });
 });
 
 
+describe("BBC", function() {
+    it("Should return that the BBC homepage was the referrer", function() {
+        //arrange
+        let ref = "www.bbc.com";
+        //act
+        let obj = new Referrer(ref, opts);
+        //assert
+        expect(obj.refferal.name).to.eql('homepage');
+    });
+
+    it("Should return the BBC CPSid of referrer", function() {
+        //arrange
+        let ref = "http://www.bbc.co.uk/news/business-37618618";
+        //act
+        let obj = new Referrer(ref, opts);
+        //assert
+        expect(obj.refferal.page).to.eql('37618618');
+        expect(obj.refferal.name).to.eql('news');
+    });
+
+    it("Should return the BBC news homepage", function() {
+        //arrange
+        let ref = "http://www.bbc.co.uk/news/";
+        //act
+        let obj = new Referrer(ref, opts);
+        //assert
+        expect(obj.refferal.page).to.eql('home');
+        expect(obj.refferal.name).to.eql('news');
+    });
+
+    it("Should return the BBC news business homepage", function() {
+        //arrange
+        let ref = "http://www.bbc.co.uk/news/business";
+        //act
+        let obj = new Referrer(ref, opts);
+        //assert
+        expect(obj.refferal.page).to.eql('business');
+        expect(obj.refferal.name).to.eql('news');
+    });
+
+    it("Should return the BBC sport homepage", function() {
+        //arrange
+        let ref = "http://www.bbc.co.uk/sport";
+        //act
+        let obj = new Referrer(ref, opts);
+        //assert
+        expect(obj.refferal.page).to.eql('home');
+        expect(obj.refferal.name).to.eql('sport');
+    });
+
+    it("Should return the BBC sport football homepage", function() {
+        //arrange
+        let ref = "http://www.bbc.co.uk/sport/football";
+        //act
+        let obj = new Referrer(ref, opts);
+        //assert
+        expect(obj.refferal.page).to.eql('football');
+        expect(obj.refferal.name).to.eql('sport');
+    });
+
+    it("Should return the BBC sport with cps id", function() {
+        //arrange
+        let ref = "http://www.bbc.co.uk/sport/live/37157487";
+        //act
+        let obj = new Referrer(ref, opts);
+        //assert
+        expect(obj.refferal.page).to.eql('37157487');
+        expect(obj.refferal.name).to.eql('sport');
+    });
+
+    it("Should return the BBC News Unknown Page", function() {
+        //arrange
+        let ref = "http://www.bbc.co.uk/news/resources/idt-6d083913-0bfb-4988-8cd8-d126fa6dcff1";
+        //act
+        let obj = new Referrer(ref, opts);
+        //assert
+        expect(obj.refferal.page).to.eql('resources/idt-6d083913-0bfb-4988-8cd8-d126fa6dcff1');
+        expect(obj.refferal.name).to.eql('news');
+    });
+
+})
